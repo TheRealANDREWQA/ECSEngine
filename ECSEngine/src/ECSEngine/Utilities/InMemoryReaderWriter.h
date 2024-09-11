0c737ed54ccb123213962eba9b0cdb434fd78c2f
TheRealANDREWQA <andreicfsteaua@gmail.com>
2024-09-11 21:24:13 +0300

    Finalized DeltaStateReader/Writer which will be the base for the input and runtime replays

--- ECSEngine/src/ECSEngine/Utilities/InMemoryReaderWriter.h
+++ ECSEngine/src/ECSEngine/Utilities/InMemoryReaderWriter.h
@@ -35,8 +35,9 @@ namespace ECSEngine {
 	struct InMemoryReadInstrument : ReadInstrument {
 		ECS_READ_INSTRUMENT_HELPER;
 
-		ECS_INLINE InMemoryReadInstrument() : buffer(nullptr), buffer_capacity(nullptr), initial_buffer(0) {}
-		ECS_INLINE InMemoryReadInstrument(uintptr_t& _buffer, size_t& _buffer_capacity) : buffer(&_buffer), buffer_capacity(&_buffer_capacity), initial_buffer(_buffer) {}
+		ECS_INLINE InMemoryReadInstrument() : buffer(nullptr), buffer_capacity(nullptr), initial_buffer(0), initial_buffer_capacity(0) {}
+		ECS_INLINE InMemoryReadInstrument(uintptr_t& _buffer, size_t& _buffer_capacity) : buffer(&_buffer), buffer_capacity(&_buffer_capacity), initial_buffer(_buffer),
+			initial_buffer_capacity(_buffer_capacity) {}
 
 		ECS_INLINE size_t GetOffset() const override {
 			return *buffer - initial_buffer;
@@ -52,8 +53,21 @@ namespace ECSEngine {
 			return true;
 		}
 
-		ECS_INLINE bool Seek(size_t offset) override {
-			*buffer = initial_buffer + offset;
+		ECS_INLINE bool Seek(SEEK_TYPE seek_type, int64_t offset) override {
+			switch (seek_type) {
+			case SEEK_START:
+				*buffer = initial_buffer + offset;
+				break;
+			case SEEK_CURRENT:
+				*buffer = *buffer + offset;
+				break;
+			case SEEK_FINISH:
+				*buffer = initial_buffer + initial_buffer_capacity + offset;
+				break;
+			default:
+				ECS_ASSERT(false);
+			}
+			
 			return true;
 		}
 
@@ -69,8 +83,13 @@ namespace ECSEngine {
 			return nullptr;
 		}
 
+		ECS_INLINE size_t TotalSize() const override {
+			return initial_buffer_capacity;
+		}
+
 		// This is the value stored at construction time
 		uintptr_t initial_buffer;
+		size_t initial_buffer_capacity;
 		uintptr_t* buffer;
 		size_t* buffer_capacity;
 	};
